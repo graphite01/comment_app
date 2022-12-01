e80ff267e3edead4b390be800b2c824cf964e67b
graphite01 <kirito10082279@gmail.com>
2022-12-01 16:21:27 +0900

    受け取った情報の確認

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,7 +1,6 @@
 import consumer from "./consumer"
 
 if(location.pathname.match(/\/items\/\d/)){
-  console.log("読み込み完了")
 
   consumer.subscriptions.create("CommentChannel", {
     connected() {
@@ -13,7 +12,7 @@ if(location.pathname.match(/\/items\/\d/)){
     },
 
     received(data) {
-
+      console.log(data)
     }
   })
 }
